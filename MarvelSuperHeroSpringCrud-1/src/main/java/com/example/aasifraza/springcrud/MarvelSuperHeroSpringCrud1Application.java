f6ec12d8b0ee0a8d1aa541ff2fdb2f0cd260d1c7
aasifcs <asifrazacse@gmail.com>
2024-08-02 18:05:57 +0530

    first Commit

---
+++ MarvelSuperHeroSpringCrud-1/src/main/java/com/example/aasifraza/springcrud/MarvelSuperHeroSpringCrud1Application.java
@@ -0,0 +1,13 @@
+package com.example.aasifraza.springcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelSuperHeroSpringCrud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelSuperHeroSpringCrud1Application.class, args);
+	}
+
+}
---
+++ MarvelSuperHeroSpringCrud-1/src/test/java/com/example/aasifraza/springcrud/MarvelSuperHeroSpringCrud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aasifraza.springcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelSuperHeroSpringCrud1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
